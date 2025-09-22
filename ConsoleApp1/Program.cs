66db990380c23b82a3b6d1989df87f77a6e73f43
Feiz0r <feizors@gmail.com>
2025-09-23 00:15:39 +0300

    2 Praktika ver 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,10 @@
-﻿internal class Program
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Numerics;
+
+class Pr1
 {
-    private static void Cal()
+    public static void Z1()
     {
         Console.WriteLine("Введите номер дня недели, с которого начинается месяц (1-пн,...7-вс)");
         int a = Convert.ToInt16(Console.ReadLine());
@@ -13,9 +17,9 @@
         Console.WriteLine("Это будет: " + weekDay[c]);
     }
 
-    private static void Bank()
+    public static void Z2()
     {
-        int[] banknote = { 5000, 2000, 1000, 500, 200, 100 };
+        int[] banknote = [5000, 2000, 1000, 500, 200, 100];
         int[] banknoteCount = new int[banknote.Length];
         Console.Write("Введите кол-во для снятия: ");
         int sum = Convert.ToInt32(Console.ReadLine());
@@ -36,11 +40,56 @@
             Console.WriteLine(banknote[i] + ": " + banknoteCount[i]);
         }
     }
+}
 
-    private static void Main(string[] args)
+class Pr2
+{
+    static double Factorial(int n)
     {
-        Cal();
-        Bank();
+        if (n <= 1)
+            return 1;
+        return n * Factorial(n - 1);
+    }
 
+    public static double Z1(double x, int n)
+    {
+        if (n == 0)       
+            return x;
+        double term = Math.Pow(-1, n) * Math.Pow(x, 2 * n + 1) / Factorial(2 * n + 1);
+        return Z1(x, n - 1) + term;
+    }
+}
+
+internal class Program
+{
+   
+    private static void Main()
+    {
+        int p = 2;
+        int z = 1;
+
+        switch (p)
+        {
+            case 1:
+                switch (z)
+                {
+                    case 1:
+                        Pr1.Z1();
+                        break;
+                    case 2:
+                        Pr1.Z2();
+                        break;
+                }
+                break;
+            case 2:
+                switch (z)
+                {
+                    case 1:
+                        double term = Pr2.Z1(Math.PI / 6, 15);
+                        Console.WriteLine(term);
+                        break;
+                }
+                break;
+        }      
     }
 }
